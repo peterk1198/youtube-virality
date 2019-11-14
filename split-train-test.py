64f12e43743b9bf08df06bfd564e4e26e2c5d0f1
unknown <dorianrm@stanford.edu>
2019-11-14 02:34:19 -0800

    Changed files to output addtional data on model. Confusion matrix, precision, recall, additional testing using dummy variable

--- split-train-test.py
+++ split-train-test.py
@@ -4,9 +4,10 @@ import json
 import numpy as np
 from tqdm import tqdm
 from collections import defaultdict
-from sklearn import datasets, linear_model
+from sklearn import datasets, linear_model, metrics
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
+from random import *
 
 # split data set in 80/10/10
 
@@ -14,6 +15,8 @@ df = pd.read_csv('./CAvirality.csv') # load the dataset as a pandas data frame
 
 data = df[['title', 'category_name']]
 y = df['viral']
+print('Exporting y to csv.')
+y.to_csv('y.csv', sep='\t')
 
 corpus = defaultdict(int)
 categories = defaultdict(int)
@@ -51,14 +54,14 @@ for index, row in tqdm(data.iterrows()):
 			temp_d[word] = 0
 	lattice.append(temp_d)
 
-print('Exporting temp_d to json.')
-with open('temp_d.json', 'w') as fp:
-	json.dump(lattice, fp, indent=4)
+# print('Exporting temp_d to json.')
+# with open('temp_d.json', 'w') as fp:
+# 	json.dump(lattice, fp, indent=4)
 
 df = pd.DataFrame(lattice)
 
-print('Exporting df to csv.')
-df.to_csv('df.csv', sep='\t')
+# print('Exporting df to csv.')
+# df.to_csv('df.csv', sep='\t')
 
 print('Creating model.')
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2)
@@ -70,16 +73,22 @@ model = lm.fit(X_train, y_train)
 print('Predicting Model')
 predictions = lm.predict(X_test)
 
-print('Plotting Model')
-plt.scatter(y_test, predictions)
-plt.xlabel('True Values')
-plt.ylabel('Predictions')
-print('Score:', model.score(X_test, y_test))
+#print('Plotting Model')
 
+print('Score:', metrics.accuracy_score(y_test, predictions))
+print('f1:', metrics.f1_score(y_test, predictions))
+print("accuracy", metrics.precision_score(y_test, predictions))
+print("recall", metrics.recall_score(y_test, predictions))
+dummy = []
+for i in range(len(predictions)):
+	dummy.append(randint(0,1))
+print('d Score:', metrics.accuracy_score(y_test, dummy))
+print('d f1:', metrics.f1_score(y_test, dummy))
+print("dummy accuracy", metrics.precision_score(y_test, dummy))
+print("d recall", metrics.recall_score(y_test, dummy))
 
-
-
-
+print("actual", confusion_matrix(y_test, predictions))
+print("dummy", confusion_matrix(y_test, dummy))
 
 
 
