a40b4088a1f4fda8e4c28437e10a9e53e9e746c0
peterk1198 <peterk11@stanford.edu>
2019-12-13 20:13:54 -0800

    Commented some code to make transitions more clear.

--- logistic-regression.py
+++ logistic-regression.py
@@ -8,16 +8,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot as plt
 
-# Logistic Regression
+# Logistic Regression.
+# This program is meant to be used after split-train-test.py. While split-train-test.py also
+# runs logistic regression, this file is where we can tune parameters, etc. when the sparse
+# matrix has been created and outputted.
 
+# opens the csv of training examples
 df = pd.read_csv('./CAvirality.csv')
 y = df['viral']
 
+# opens the dataframe that is exported from split-train-test.py
 df = pd.read_csv('./df.csv')
 
+# creates scikit learn model.
 print('Creating model.')
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2)
 
+# fits model.
 print('Made Model')
 lm = linear_model.LogisticRegression()
 print('Fitting Model')
@@ -25,6 +32,7 @@ model = lm.fit(X_train, y_train)
 print('Predicting Model')
 predictions = lm.predict(X_test)
 
+# plots models predictions, etc.
 print('Plotting Model')
 plt.scatter(y_test, predictions)
 plt.xlabel('True Values')
--- split-train-test.py
+++ split-train-test.py
@@ -8,6 +8,7 @@ from sklearn import datasets, linear_model, metrics
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 from random import *
+from sklearn.metrics import confusion_matrix
 
 # split data set in 80/10/10
 
@@ -36,6 +37,7 @@ print('######### Json Dumped #########')
 
 lattice = []
 
+# creates sparse matrix. writes a 1 if the word or category exists, else a 0.
 for index, row in tqdm(data.iterrows()):
 	temp_d = {}
 	T = re.sub(r'\W+', ' ', row['title'])
@@ -54,14 +56,14 @@ for index, row in tqdm(data.iterrows()):
 			temp_d[word] = 0
 	lattice.append(temp_d)
 
-# print('Exporting temp_d to json.')
-# with open('temp_d.json', 'w') as fp:
-# 	json.dump(lattice, fp, indent=4)
+print('Exporting temp_d to json.')
+with open('temp_d.json', 'w') as fp:
+	json.dump(lattice, fp, indent=4)
 
 df = pd.DataFrame(lattice)
 
-# print('Exporting df to csv.')
-# df.to_csv('df.csv', sep='\t')
+print('Exporting df to csv.')
+df.to_csv('df.csv', sep='\t')
 
 print('Creating model.')
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2)
@@ -73,22 +75,24 @@ model = lm.fit(X_train, y_train)
 print('Predicting Model')
 predictions = lm.predict(X_test)
 
-#print('Plotting Model')
+# prints the confusion matrix of our model.
 
-print('Score:', metrics.accuracy_score(y_test, predictions))
-print('f1:', metrics.f1_score(y_test, predictions))
-print("accuracy", metrics.precision_score(y_test, predictions))
-print("recall", metrics.recall_score(y_test, predictions))
+print('Accuracy:', metrics.accuracy_score(y_test, predictions))
+print('F1 Score:', metrics.f1_score(y_test, predictions))
+print("Precision", metrics.precision_score(y_test, predictions))
+print("Recall", metrics.recall_score(y_test, predictions))
+
+# writes and plots how randomly guessing a 0 or 1 performs in comparison to our model.
 dummy = []
 for i in range(len(predictions)):
 	dummy.append(randint(0,1))
-print('d Score:', metrics.accuracy_score(y_test, dummy))
-print('d f1:', metrics.f1_score(y_test, dummy))
-print("dummy accuracy", metrics.precision_score(y_test, dummy))
-print("d recall", metrics.recall_score(y_test, dummy))
+print('d Accuracy:', metrics.accuracy_score(y_test, dummy))
+print('d F1 Score:', metrics.f1_score(y_test, dummy))
+print("d Precision", metrics.precision_score(y_test, dummy))
+print("d Recall", metrics.recall_score(y_test, dummy))
 
-print("actual", confusion_matrix(y_test, predictions))
-print("dummy", confusion_matrix(y_test, dummy))
+print("Model Confusion Matrix", confusion_matrix(y_test, predictions))
+print("Dummy Confusion Matrix", confusion_matrix(y_test, dummy))
 
 
 
